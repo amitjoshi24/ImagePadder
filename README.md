cd6fae68a98bffbf962c40e9a5bdc35626789c5d
Jeffrey Wang <j@mywikis.com>
2022-05-27 17:22:32 -0500

    Quality of life improvements

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 keeping full resolution, pads image to be whatever aspect ratio you want, names it "padded" + the original file name
+
+## Installation
+Python 3.4+ is required to run this.
+
+No virtual environment is required for running this. However, if this makes you feel more comfortable, you can set one up by using the following code:
+
+1. ```bash
+python3 -m venv .venv
+```
+2. ```bash
+source .venv/bin/activate
+```
+
+## Usage
+The image padder can be ran either by passing in command-line arguments or by using the interactive prompt.
+
+'''CLI arguments''':
+```bash
+python3 imagepadder2.py '/Users/apple/Pictures/best_friends_photo.jpg' 16x9
+```
+
+'''Interactive prompt''':
+```bash
+python3 imagepadder2.py
+```
+```
+> image's filename to pad: /Users/apple/Pictures/best_friends_photo.jpg
+> enter aspect ratio you desire in the form of WIDTHxHEIGHT, e.g. "4x3": 16x9
+```
--- imagepadder2.py
+++ imagepadder2.py
@@ -2,33 +2,45 @@
 from PIL import Image
 import sys
 from math import ceil
+from pathlib import Path
 
-file = input("image's filename to pad:")
+def pad_image(file_path: Path, nw: int, nh: int):
+    width, height = im.size
 
-im = Image.open(file)
+    padded_save_path = file_path.parent / (file_path.stem + '-padded_' + str(nw) + 'x' + str(nh) + file_path.suffix)
 
-aspectRatio = input("enter aspect ratio you desire in the form of WIDTHxHEIGHT, i.e. \"4x3\":")
+    vertical_pad = nw/nh <= width/height
 
-splitted = aspectRatio.split("x")
-nw = int(splitted[0])
-nh = int(splitted[1])
-width, height = im.size
+    new_width = width if vertical_pad else int(ceil(height * nw / nh))
+    new_height = int(ceil(width * nh / nw)) if vertical_pad else height
 
-if nw/nh <= width/height:
-	# vertical pad
-	newwidth = width
-	newheight = int(ceil(width * nh / nw))
+    result = Image.new(im.mode, (new_width, new_height), (255, 255, 255))
 
-	result = Image.new(im.mode, (newwidth, newheight), (255, 255, 255))
-	startheight = (newheight - height)//2
-	result.paste(im, (0, startheight))
-	result.save("padded" + str(nw) + "x" + str(nh) + file)
-else:
-	# horizontal pad
-	newheight = height
-	newwidth = int(ceil(height * nw / nh))
+    start_length = ((new_height - height) // 2) if vertical_pad else ((new_width - width) // 2)
+    if vertical_pad:
+        result.paste(im, (0, start_length))
+    else:
+        result.paste(im, (start_length, 0))
 
-	result = Image.new(im.mode, (newwidth, newheight), (255, 255, 255))
-	startwidth = (newwidth - width)//2
-	result.paste(im, (startwidth, 0))
-	result.save("padded" + str(nw) + "x" + str(nh) + file)
+    result.save(padded_save_path)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        raw_file_path = sys.argv[1]
+        aspect_ratio = sys.argv[2]
+    else:
+        raw_file_path = input("image's filename to pad: ")
+        aspect_ratio = input("enter aspect ratio you desire in the form of WIDTHxHEIGHT, e.g. \"4x3\": ")
+
+    file_path = Path(raw_file_path)
+    try:
+        im = Image.open(raw_file_path)
+    except:
+        print('Image cannot be opened at this path: ' + raw_file_path)
+    
+    try:
+        nw, nh = tuple(int(num) for num in aspect_ratio.split('x'))
+    except:
+        print('Invalid aspect ratio. Must be in the format WIDTHxHEIGHT, e.g. \"4x3\".')
+
+    pad_image(file_path, nw, nh)
